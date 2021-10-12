7c993a189bef75f3561c2fcdc49963723f069b40
hoang0308 <hoangph030898@gmail.com>
2021-10-12 12:51:19 +0700

    add ex2.rb

--- ex2.rb
+++ ex2.rb
@@ -4,19 +4,8 @@ arr = Array.new(n)
 for i in 0...n do
     arr[i] = gets.chomp.to_i
 end
-new = []
-new.push(arr[0])
-b = true
-for i in 0...n do
-    b = true
-    for num in new do
-        if num == arr[i]
-            b = false
-            break
-        end
-    end
-    if b
-        new.push(arr[i])
-    end
-end
+
+h = Hash.new(0)
+arr.map{|v| h[v] +=1}
+new = h.keys
 print new
